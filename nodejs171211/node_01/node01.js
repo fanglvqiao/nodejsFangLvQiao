a8f7a2f9c7cb7ec875a237a9e079db3aa89281f1
dllo <1348463438@qq.com>
2018-03-26 21:07:39 +0800

    {提交人}:方侣桥 {提交时间}2018.3.26 {提交内容}作业

--- nodejs171211/node_01/node01.js
+++ nodejs171211/node_01/node01.js
@@ -1,1 +1,20 @@
-//奥术大师大所多
+console.log("hello node");
+
+
+
+{
+    var a = 10;
+    let b = 11;//let有作用域 只在当前代码段有效
+    console.log(b);
+}
+console.log(a);
+//console.log(b);
+
+
+var arr = [];
+for (let i = 0; i < 10; i++){
+    arr[i] = function () {
+        console.log(i)
+    }
+}
+console.log(arr[6])
